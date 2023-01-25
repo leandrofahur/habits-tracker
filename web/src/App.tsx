a464efb8e461435e7bbd97d5af03c2cd6122b596
leandro <fahurleandro@gmail.com>
2023-01-24 18:35:02 -0800

    tailwind setup

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,13 @@
 import { Habit } from "./components/Habit/Habit.component";
+import './styles/global.css'
 
 export function App() {
   return (
-    <div><Habit/></div>
+    <div>
+      <Habit completed={0}/>
+      <Habit completed={1}/>
+      <Habit completed={2}/>
+      <Habit completed={3}/>      
+    </div>
   )
 }
--- web/src/components/Habit/Habit.component.tsx
+++ web/src/components/Habit/Habit.component.tsx
@@ -1,7 +1,11 @@
 interface HabitProps {
-  completed: boolean
+  completed: number
 }
 
 export function Habit(props : HabitProps) {
-  return <div>Habit Component</div>
+  const { completed} = props;
+
+  return <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 text-center flex items-center justify-center">
+    {completed}
+  </div>
 }
